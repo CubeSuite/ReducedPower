ca2a6eba2eb1f49d13c62a51ac97a4a9763638e3
CubeSuite <cubesuite@outlook.com>
2024-04-04 19:01:28 +0100

    Add project files.

---
+++ ReducedPower/Patches/PlanterInstancePatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace ReducedPower.Patches
+{
+    internal class PlanterInstancePatch
+    {
+        [HarmonyPatch(typeof(PlanterInstance), "SimUpdate")]
+        [HarmonyPostfix]
+        static void reducePower(PlanterInstance __instance) {
+            Debug.Log("Setting power to 50");
+            __instance.powerInfo.maxPowerConsumption = 50;
+        }
+    }
+}
---
+++ ReducedPower/ReducedPowerPlugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using EquinoxsModUtils;
+using HarmonyLib;
+using ReducedPower.Patches;
+using System;
+using UnityEngine;
+
+namespace ReducedPower
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class ReducedPowerPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.ReducedPower";
+        private const string PluginName = "ReducedPower";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake() {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+
+            ModUtils.GameDefinesLoaded += OnGameDefinesLoaded;
+
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+        }
+
+        private void OnGameDefinesLoaded(object sender, EventArgs e) {
+            BuilderInfo planterInfo = (BuilderInfo)ModUtils.GetResourceInfoByName(ResourceNames.Planter);
+            planterInfo.kWPowerConsumption = 12;
+
+            BuilderInfo thresherInfo = (BuilderInfo)ModUtils.GetResourceInfoByName(ResourceNames.Thresher);
+            thresherInfo.kWPowerConsumption = 50;
+        }
+    }
+}
